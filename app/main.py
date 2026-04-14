976a69482241ff79f676561074dd5a8861618045
francesshenaiclass <francesshen.aiclass@gmail.com>
2026-04-14 16:21:08 +0800

    Revert "Update application version to v5.1"
    This reverts commit 6eea2118b0ff1fa0de5bb5b066836319ac3e8a95.

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "version": os.getenv("APP_VERSION", "v5.1"),
+        "version": os.getenv("APP_VERSION", "v5.0"),
         "status": "ok",
     }
