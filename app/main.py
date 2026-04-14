8c64ccdd27f680b63baea9ad0808fa560279afaf
francesshenaiclass <francesshen.aiclass@gmail.com>
2026-04-14 14:02:59 +0800

    Update app version to v3.0

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "version": os.getenv("APP_VERSION", "v1.0"),
+        "version": os.getenv("APP_VERSION", "v3.0"),
         "status": "ok",
     }
